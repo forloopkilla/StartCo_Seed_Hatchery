323e0b26dbaad4157be24de8956020bc28cd5342
arpanajax <arpan.soni@ajaxintel.com>
2017-03-27 15:58:58 +0530

    Update Twitter_Scrape.R
    Have updated the For Loop for pulling with the different Twitter APIs.

--- Twitter_Scrape.R
+++ Twitter_Scrape.R
@@ -1,18 +1,8 @@
 getwd()
-setwd("/home/arpan/Documents/Ajax Intel/Sentiment_analysis ")
+setwd("/home/arpan/Documents/Ajax Intel/Sentiment_analysis")
 
-#-----DOWNLOAD THE STOCKS FILE-----
-URL_prod_rev <- 
-  "https://raw.githubusercontent.com/datasets/s-and-p-500-companies/master/data/constituents-financials.csv"
-SP500 <- read.csv(URL_prod_rev, 
-                  header=T, 
-                  sep=",",
-                  stringsAsFactors=FALSE)
+#-----SAVE THE APIs------
 
-#
-
-SP500_tickers <- paste("$",SP500$Symbol, sep = "")
-SP500_tickers <- paste(SP500_tickers,SP500$Name, sep = " ")
 
 consumer_key<- c("x8GzVDZNm376fDEhhgUjeDdLc",	"BaXijPbyZTBtmjKsXVDPwTPCc",
                  "AKJsxNqX2D8uTo9orgjRirvWL",	"Ch6e19OXkGNeUP8QBYUIr5WRH",	"IOhPOwbSc5eBFq5uDVSBHf6Gb",
@@ -35,65 +25,69 @@ access_secret <-c("1ZG2StMVZ5XuN0GG7OTQ6ZEPuCCO8wnbxnlAGo5xS58sG",	"s0kTmeiautNW
                   "S1lf38C5Ax9Q1ZHuVrvxDTjak7YNo1va8AoBEF9ZPd63B",	"LepBEZIBMZwllWMRl8TjoTK3KBtJEYud4A76AQ1uIRs4x",	
                   "vFdBepLoljrHItEZ315bT9Lsqy2qYxfrCsdbYoP5PcTcW",	"DCyejp7s8BFCjdP0TM4AkOwYzfvuG4HxAgR4L5AKRM7t7")	
 
-setupCrawler <- function(consumer_key,consumer_secret,access_token,access_secret) {
+
+#-----DOWNLOAD THE STOCKS FILE-----
+URL_prod_rev <- 
+  "https://raw.githubusercontent.com/datasets/s-and-p-500-companies/master/data/constituents-financials.csv"
+SP500 <- read.csv(URL_prod_rev, 
+                  header=T, 
+                  sep=",",
+                  stringsAsFactors=FALSE)
+
+#
+
+SP500_tickers <- paste("$",SP500$Symbol, sep = "")
+SP500_tickers <- paste(SP500_tickers,SP500$Name, sep = " ")
+
+# -----ACCESS THE TWITTER ACCOUNTS WITH API CALLS----
+twitter_access <- function(consumer_key,consumer_secret,access_token,access_secret){
   
   library(ROAuth)
   library(twitteR)
   setup_twitter_oauth(consumer_key ,consumer_secret, access_token,  access_secret )
+  
+  cred <- OAuthFactory$new(consumerKey = consumer_key, 
+                           consumerSecret = consumer_secret,
+                           requestURL='https://api.twitter.com/oauth/request_token',
+                           accessURL='https://api.twitter.com/oauth/access_token',
+                           authURL='https://api.twitter.com/oauth/authorize')
+
+  return(TRUE) 
+}  
+
+
+# ------TWITTER CRAWLER FUNCTION TO PULL DATA-----------
+
+twitter_crawler <- function(crawl_j){
+
+  tweets <<- list()
+  crawl_begin <-((crawl_j-1)*63)+1
+  crawl_end <- crawl_j*63
+  
+  for (i in SP500_tickers[crawl_begin:crawl_end]) {
+    tweets[i] <<- list(searchTwitter(i,n=1 ,lang = "en", since = '2017-03-20'))
+  } 
 }
 
-setupCrawler()
-  #cred <- OAuthFactory$new(consumerKey = consumer_key, 
-   #                        consumerSecret = consumer_secret,
-    #                       requestURL='https://api.twitter.com/oauth/request_token',
-     #                      accessURL='https://api.twitter.com/oauth/access_token',
-      #                     authURL='https://api.twitter.com/oauth/authorize')
 
-  tweets<- list()
-  
-  for (i in SP500_tickers[1:3]) {
-    tweets[i] <- list(searchTwitter(i,n=3 ,lang = "en", since = '2017-03-22'))
-  }
-  
+twitter_df <- function(){
+
   tweets<- unlist(tweets)
   #convert to data frame
-  df<- do.call("rbind", lapply(tweets,as.data.frame))
-  #retweet<- df[length(which(df$retweetCount>0 | df$favoriteCount >0)),]
-  #retweet$text <- sapply(retweet$text, function(row) iconv(row, "latin1","ASCII",sub = ""))
-  #retweet$text <- gsub("(f|ht)tp(s?)://(.*)[.][a-z]+","",retweet$text)
-  
-  raw.sample <- df$text
-  
-  #  -------Save log file------
-  con <- file("test.log")
-  sink(con, append=TRUE)
-  sink(con, append=TRUE, type="message")
-  
-  # This will echo all input and not truncate 150+ character lines...
-  source("script.R", echo=TRUE, max.deparse.length=10000)
-  
-  # Restore output to console
-  sink() 
-  sink(type="message")
-  
-  # And look at the log...
-  cat(readLines("test.log"), sep="\n")
-  
-  return(df)
-  
+  tweet_df <<- do.call("rbind", lapply(tweets,as.data.frame))
+  return(tweet_df)
+
 }
 
+final_df <- data.frame(names=NULL)
 
 
-setupCrawler("x8GzVDZNm376fDEhhgUjeDdLc" ,"l2vc71gcyo2i213kXyLv4ZBNSjQO9hmburVwkyIrEdFQsg395C", "843639734782119937-tdHQ7GTqMEl40Zd5MJgfgweN0uivqY2",  "1ZG2StMVZ5XuN0GG7OTQ6ZEPuCCO8wnbxnlAGo5xS58sG")
-
-
-master_df <- list()
-for (i in 6:8) {
-  master_df<- setupCrawler(consumer_key[i] ,consumer_secret[i], access_token[i],  access_secret[i] )
-console.log(setupCrawler)
+for(i in 1:8){
+  twitter_access(consumer_key[i] ,consumer_secret[i], access_token[i],  access_secret[i] )
+  twitter_crawler(i)
+  twitter_df()
+  final_df <- rbind(final_df, tweet_df)
 }
-
 
 
 
